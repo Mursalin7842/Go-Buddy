aa07d990b46043b91aaf4e9a384deead0396850f
Mursaline Huqe <mursalin7842@gmail.com>
2025-07-12 03:40:07 +0600

    feat: Implement insert function in ProjectDao
    This commit introduces the `insert` function to the `ProjectDao` class. This function will be used to insert `Project` objects into the database.
    
    The `.idea/misc.xml` file was also updated, likely due to IDE configuration changes.

--- app/src/main/java/mursalin/companion/gobuddy/data/local/dao/ProjectDao.kt
+++ app/src/main/java/mursalin/companion/gobuddy/data/local/dao/ProjectDao.kt
@@ -1,4 +1,9 @@
 package mursalin.companion.gobuddy.data.local.dao
 
+import mursalin.companion.gobuddy.domain.model.Project
+
 class ProjectDao {
+    fun insert(project: Project) {
+        // Insert the project into the database
+    }
 }
