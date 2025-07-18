bd4136b5c38d1ae3f8638308545793b2082d58dc
Mursaline Huqe <mursalin7842@gmail.com>
2025-07-19 02:05:57 +0600

    feat: Update app theme colors
    This commit updates the color schemes for both dark and light themes in the GoBuddy app. The new colors provide a more visually distinct and consistent look and feel across different theme modes.

--- app/src/main/java/mursalin/companion/gobuddy/presentation/theme/Theme.kt
+++ app/src/main/java/mursalin/companion/gobuddy/presentation/theme/Theme.kt
@@ -8,18 +8,34 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+
+        primary = Color(0xFFBB86FC),
+        secondary = Color(0xFF03DAC6),
+        tertiary = Color(0xFF3700B3),
+        background = Color(0xFF121212),
+        surface = Color(0xFF1E1E1E),
+        onPrimary = Color.Black,
+        onSecondary = Color.Black,
+        onTertiary = Color.White,
+        onBackground = Color.White,
+        onSurface = Color.White,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Color(0xFF6200EE),
+    secondary = Color(0xFF03DAC6),
+    tertiary = Color(0xFF3700B3),
+    background = Color(0xFFFFFFFF),
+    surface = Color(0xFFFFFFFF),
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onTertiary = Color.White,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -31,6 +47,8 @@ private val LightColorScheme = lightColorScheme(
     onSurface = Color(0xFF1C1B1F),
     */
 )
+
+
 
 @Composable
 fun GoBuddyTheme(
