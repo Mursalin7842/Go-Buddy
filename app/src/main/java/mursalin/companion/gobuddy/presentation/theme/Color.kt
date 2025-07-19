da6ebd5906c182d41e4a00f04d9f6c0a43c8c170
Mursaline Huqe <mursalin7842@gmail.com>
2025-07-20 02:55:43 +0600

    feat: Implement basic DashboardScreen and dark theme colors
    - Created a simple `DashboardScreen` composable function that displays a "Welcome to GO Buddy!" message.
    - Defined a set of dark theme colors (`DarkPrimary`, `DarkSecondary`, etc.) in `Color.kt`.

--- app/src/main/java/mursalin/companion/gobuddy/presentation/theme/Color.kt
+++ app/src/main/java/mursalin/companion/gobuddy/presentation/theme/Color.kt
@@ -2,19 +2,14 @@ package mursalin.companion.gobuddy.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
-val Primary = Color(0xFF6A1B9A) // A deep purple
-val Secondary = Color(0xFFF06292) // A vibrant pink
-val Tertiary = Color(0xFF4DD0E1) // A bright cyan
-val Background = Color(0xFFF5F5F5) // A light grey for the background
-val OnBackground = Color(0xFF212121) // Dark text on light background
-val Surface = Color(0xFFFFFFFF) // White for card backgrounds
-val OnSurface = Color(0xFF212121) // Dark text on surfaces
-val OnSurfaceVariant = Color(0xFF616161) // Grey for less important text
+// Dark Theme Colors
+val DarkPrimary = Color(0xFFBB86FC)
+val DarkSecondary = Color(0xFF03DAC6)
+val DarkTertiary = Color(0xFF3700B3)
+val DarkBackground = Color(0xFF121212)
+val DarkSurface = Color(0xFF1E1E1E)
+val DarkOnPrimary = Color.Black
+val DarkOnSecondary = Color.Black
+val DarkOnTertiary = Color.White
+val DarkOnBackground = Color.White
+val DarkOnSurface = Color.White
