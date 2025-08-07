c73a902a93e02e29419b7d1c977724f4db345b3e
Mursaline Huqe <mursalin7842@gmail.com>
2025-08-08 02:40:56 +0600

    Refactor: Remove trailing newline from GetProjectUseCase.kt
    This commit removes an unnecessary trailing newline character from the `GetProjectUseCase.kt` file. This change has no functional impact on the code.

--- app/src/main/java/mursalin/companion/gobuddy/presentation/components/LoadingAnimation.kt
+++ app/src/main/java/mursalin/companion/gobuddy/presentation/components/LoadingAnimation.kt
@@ -1,16 +1,20 @@
 package mursalin.companion.gobuddy.presentation.components
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
+/**
+ * A reusable loading animation component.
+ *
+ * @param modifier The modifier to be applied to the layout.
+ */
 @Composable
-fun LoadingAnimation() {
+fun LoadingAnimation(modifier: Modifier = Modifier) {
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = modifier,
         contentAlignment = Alignment.Center
     ) {
         CircularProgressIndicator()
